561c49bef506ad25884921f116880033a801d36b
vganesh <velganesh_ashokkumaar@trimble.com>
2014-02-27 18:53:44 +0530

    Version 1 is ready

--- src/main/Script3.java
+++ src/main/Script3.java
@@ -3,8 +3,7 @@ package main;
 public class Script3 {
 
 	public static void main(String[] args) {
-		System.out.println("Sample Git Script 10!!!");
-		System.out.println("Sample Git Script 11!!!");
+		System.out.println("Release Version 1.0 is ready !!");
 	}
 
 }
