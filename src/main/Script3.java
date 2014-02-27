65e4aafd2224997f4400a48cf026caa4277027cf
vganesh <velganesh_ashokkumaar@trimble.com>
2014-02-27 19:20:23 +0530

    Developer Group 01 changes 02

--- src/main/Script3.java
+++ src/main/Script3.java
@@ -5,6 +5,7 @@ public class Script3 {
 	public static void main(String[] args) {
 		System.out.println("Release Version 1.0 is ready !!");
 		System.out.println("Developer Group 01 changes !!");
+		System.out.println("Developer Group 01 changes !!");
 	}
 
 }
